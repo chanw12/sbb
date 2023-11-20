6e434ddbd12f23ba5a3854def786705517b2544f
chanw12 <wooju9812127@gmail.com>
2023-11-20 13:23:59 +0900

    querydsl 적용 및 테스트

--- src/test/java/com/ll/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/sbb/SbbApplicationTests.java
@@ -1,13 +1,31 @@
 package com.ll.sbb;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@Transactional
 class SbbApplicationTests {
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query.selectFrom(qHello).fetchOne();
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }

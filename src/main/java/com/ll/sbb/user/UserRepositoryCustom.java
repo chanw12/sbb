308bf7fd5a7b79545777d411f4d017cfd4bc28a4
chanw12 <wooju9812127@gmail.com>
2023-11-24 13:14:01 +0900

    3-07

--- src/main/java/com/ll/sbb/user/UserRepositoryCustom.java
+++ src/main/java/com/ll/sbb/user/UserRepositoryCustom.java
@@ -1,5 +1,8 @@
 package com.ll.sbb.user;
 
+import java.util.Optional;
+
 public interface UserRepositoryCustom {
 
+    Optional<SiteUser> findByusername(String username);
 }
--- src/main/java/com/ll/sbb/user/UserRepositoryImpl.java
+++ src/main/java/com/ll/sbb/user/UserRepositoryImpl.java
@@ -1,6 +1,29 @@
 package com.ll.sbb.user;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
 public class UserRepositoryImpl implements UserRepositoryCustom {
 
+    private final JPAQueryFactory jpaQueryFactory;
 
+    public UserRepositoryImpl(EntityManager em) {
+        this.jpaQueryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Optional<SiteUser> findByusername(String username) {
+        QSiteUser qSiteUser = QSiteUser.siteUser;
+
+
+        return Optional.ofNullable(jpaQueryFactory.selectFrom(qSiteUser)
+                .where(qSiteUser.username.eq(username))
+                .fetchOne());
+    }
 }
